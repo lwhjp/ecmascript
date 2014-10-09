ea9ad96495931d61d574ccd757189d9449608957
Leo Uino <leo@norisys.jp>
2014-10-09 16:37:06 +0900

    Automatic semicolon insertion and parser tidy-up

--- private/grammar.rkt
+++ private/grammar.rkt
@@ -1,68 +1,72 @@
 #lang ragg
 
-program: (statement | function-declaration)+
+program: (ws statement | ws function-declaration)+ ws
+
+ws: (COMMENT | EOL | WS)*
+ws-no-eol: (COMMENT | WS)*
+eol-or-semicolon: EOL | ";"
 
 ;; Expressions
 
-primary-expression: "this" | identifier | numeric | string | "null" | "true" | "false" | "(" expression ")"
+primary-expression: "this" | identifier | numeric | string | "null" | "true" | "false" | "(" ws expression ws ")"
 identifier: IDENTIFIER
 numeric: NUMERIC
 string: STRING
 member-expression:
     primary-expression |
-    member-expression "[" expression "]" |
-    member-expression "." identifier |
-    "new" member-expression arguments
+    member-expression ws "[" ws expression ws "]" |
+    member-expression ws "." ws identifier |
+    "new" ws member-expression ws arguments
 new-expression:
     member-expression |
-    "new" new-expression
+    "new" ws new-expression
 call-expression:
-    member-expression arguments |
-    call-expression arguments |
-    call-expression "[" expression "]" |
-    call-expression "." identifier
-arguments: "(" [ assignment-expression [ "," assignment-expression ]* ] ")"
+    member-expression ws arguments |
+    call-expression ws arguments |
+    call-expression ws "[" ws expression ws "]" |
+    call-expression ws "." ws identifier
+arguments: "(" [ ws assignment-expression [ ws "," ws assignment-expression ]* ] ws ")"
 left-hand-side-expression: new-expression | call-expression
-postfix-expression: left-hand-side-expression [ "++" | "--" ]
-unary-expression: postfix-expression | ("delete" | "void" | "typeof" | "++" | "--" | "+" | "-" | "~" | "!") unary-expression
-multiplicative-expression: [multiplicative-expression ("*" | "/" | "%")] unary-expression
-additive-expression: [additive-expression ("+" | "-")] multiplicative-expression
-shift-expression: [shift-expression ("<<" | ">>" | ">>>")] additive-expression
-relational-expression: [relational-expression ("<" | ">" | "<=" | ">=")] shift-expression
-equality-expression: [equality-expression ("==" | "!=")] relational-expression
-bitwise-and-expression: [bitwise-and-expression "&"] equality-expression
-bitwise-xor-expression: [bitwise-xor-expression "^"] bitwise-and-expression
-bitwise-or-expression: [bitwise-or-expression "|"] bitwise-xor-expression
-logical-and-expression: [logical-and-expression "&&"] bitwise-or-expression
-logical-or-expression: [logical-or-expression "||"] logical-and-expression
-conditional-expression: logical-or-expression ["?" assignment-expression ":" assignment-expression]
-assignment-expression: conditional-expression | left-hand-side-expression assignment-operator assignment-expression
+postfix-expression: left-hand-side-expression ws-no-eol [ "++" | "--" ]
+unary-expression: postfix-expression | ("delete" | "void" | "typeof" | "++" | "--" | "+" | "-" | "~" | "!") ws unary-expression
+multiplicative-expression: [multiplicative-expression ws ("*" | "/" | "%") ws] unary-expression
+additive-expression: [additive-expression ws ("+" | "-") ws] multiplicative-expression
+shift-expression: [shift-expression ws ("<<" | ">>" | ">>>") ws] additive-expression
+relational-expression: [relational-expression ws ("<" | ">" | "<=" | ">=") ws] shift-expression
+equality-expression: [equality-expression ws ("==" | "!=") ws] relational-expression
+bitwise-and-expression: [bitwise-and-expression ws "&" ws] equality-expression
+bitwise-xor-expression: [bitwise-xor-expression ws "^" ws] bitwise-and-expression
+bitwise-or-expression: [bitwise-or-expression ws "|" ws] bitwise-xor-expression
+logical-and-expression: [logical-and-expression ws "&&" ws] bitwise-or-expression
+logical-or-expression: [logical-or-expression ws "||" ws] logical-and-expression
+conditional-expression: logical-or-expression [ ws "?" ws assignment-expression ws ":" ws assignment-expression]
+assignment-expression: conditional-expression | left-hand-side-expression ws assignment-operator ws assignment-expression
 assignment-operator: "=" | "*=" | "/=" | "%=" | "+=" | "-=" | "<<=" | ">>=" | ">>>=" | "&=" | "^=" | "|="
-expression: [expression ","] assignment-expression
+expression: [expression ws "," ws] assignment-expression
 
 ;; Statements
 
 statement: block | variable-statement | empty-statement | expression-statement |
     if-statement | while-statement | for-statement | continue-statement |
     break-statement | return-statement | with-statement
-block: "{" statement* "}"
-variable-statement: "var" variable-declaration-list ";"
-variable-declaration-list: variable-declaration ["," variable-declaration]*
-variable-declaration: identifier [initializer]
-initializer: "=" expression
+block: "{" [ws statement]* ws "}"
+variable-statement: "var" ws variable-declaration-list eol-or-semicolon
+variable-declaration-list: variable-declaration [ws "," ws variable-declaration]*
+variable-declaration: identifier [ws initializer]
+initializer: "=" ws expression
 empty-statement: ";"
-expression-statement: expression ";"
-if-statement: "if" "(" expression ")" statement ["else" statement]
-while-statement: "while" "(" expression ")" statement
+expression-statement: expression eol-or-semicolon
+if-statement: "if" ws "(" ws expression ws ")" ws statement [ws "else" ws statement]
+while-statement: "while" ws "(" ws expression ws ")" ws statement
 for-statement:
-    "for" "(" ("var" variable-declaration-list | [expression]) ";" [expression] ";" [expression] ")" statement |
-    "for" "(" ("var" identifier [initializer] | expression) "in" expression ")" statement
-continue-statement: "continue" ";"
-break-statement: "break" ";"
-return-statement: "return" [expression] ";"
-with-statement: "with" "(" expression ")" statement
+    "for" ws "(" ws ("var" ws variable-declaration-list | [expression]) ws ";" [ws expression] ws ";" [ws expression] ws ")" ws statement |
+    "for" ws "(" ws ("var" ws identifier [ws initializer] | expression) ws "in" ws expression ws ")" ws statement
+continue-statement: "continue" eol-or-semicolon
+break-statement: "break" eol-or-semicolon
+return-statement: "return" ws-no-eol [expression] eol-or-semicolon
+with-statement: "with" ws "(" ws expression ws ")" ws statement
 
 ;; Function definition
 
-function-declaration: "function" identifier "(" [formal-parameter-list] ")" block
-formal-parameter-list: identifier ("," identifier)*
+function-declaration: "function" ws identifier ws "(" ws formal-parameter-list ws ")" ws block
+formal-parameter-list: identifier (ws "," ws identifier)*
--- private/parse.rkt
+++ private/parse.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (require parser-tools/lex
-         racket/sequence
+         racket/generator
+         racket/list
          syntax/parse
          syntax/stx
          "grammar.rkt"
@@ -13,21 +14,65 @@
 (define (in-tokens in)
   (in-producer es-lex 'EOF in))
 
-(define (filter-whitespace t)
-  (not
-   (memq
-    (position-token-token t)
-    '(COMMENT EOL WS))))
+(define (insert-block-semicolons seq)
+  (define-values (has-more? next)
+    (sequence-generate seq))
+  (define (needs-insert? token)
+    (not
+     (member token '(EOL ";" "{" "}"))))
+  (in-generator
+   (let loop ([last-non-ws #f])
+     (cond
+       [(has-more?)
+        (let* ([pt (next)]
+               [t (position-token-token pt)])
+          (when (and (equal? "}" t)
+                     (needs-insert? last-non-ws))
+            (yield '";"))
+          (yield pt)
+          (loop (if (memq t '(COMMENT WS))
+                    last-non-ws
+                    t)))]
+       [else
+        (when (needs-insert? last-non-ws)
+          (yield ";"))]))))
 
 (define (port->raw-syntax in)
   (parse
-   (sequence-filter
-    filter-whitespace
+   (insert-block-semicolons
     (in-tokens in))))
 
+(define (strip-whitespace stx)
+  (syntax-parse stx
+    [(rule sub ...)
+     #`(rule
+        #,@(filter-map
+            (λ (stx)
+              (syntax-parse stx
+                [((~datum ws) . _) #f]
+                [((~datum ws-no-eol) . _) #f]
+                [_ (strip-whitespace stx)]))
+            (syntax->list #'(sub ...))))]
+    [_ stx]))
+
+(define (collapse-syntax stx)
+  (syntax-parse stx
+    [((~datum eol-or-semicolon) _) #'";"]
+    [((~not (~or (~datum variable-declaration-list)
+                 (~datum variable-declaration)
+                 (~datum formal-parameter-list)))
+      (~and (_ ...) sub))
+     (collapse-syntax #'sub)]
+    [(rule sub ...)
+     #`(rule #,@(map collapse-syntax (syntax->list #'(sub ...))))]
+    [_ stx]))
+
 (define (es-parse in)
-  (parse-program
-   (port->raw-syntax in)))
+  (define stx
+    (collapse-syntax
+     (strip-whitespace
+      (port->raw-syntax in))))
+  (parse-program stx))
 
 (define (parse-program stx)
   (syntax-parse stx
@@ -36,47 +81,39 @@
 
 (define (parse-source-element stx)
   (syntax-parse stx
-    #:datum-literals (statement function-declaration)
-    [(statement stmt) (parse-statement #'stmt)]
-    [(function-declaration . _) (parse-function stx)]))
+    #:datum-literals (function-declaration)
+    [(function-declaration . _) (parse-function stx)]
+    [_ (parse-statement stx)]))
 
 (define (parse-expression stx)
   (syntax-parse stx
-    #:datum-literals (primary-expression member-expression new-expression
-                      call-expression arguments left-hand-side-expression
-                      postfix-expression unary-expression multiplicative-expression
-                      additive-expression shift-expression relational-expression
-                      equality-expression bitwise-and-expression bitwise-xor-expression
-                      bitwise-or-expression logical-and-expression logical-or-expression
-                      conditional-expression assignment-expression assignment-operator
-                      expression)
-    [(primary-expression . _) (parse-primary-expression stx)]
+    #:datum-literals (primary-expression identifier numeric string
+                      comma-expression member-expression new-expression
+                      call-expression postfix-expression unary-expression
+                      conditional-expression assignment-expression
+                      assignment-operator expression)
+    [(primary-expression "this") (es-this stx)]
+    [(identifier id) (es-id #'id (syntax-e #'id))]
+    [(numeric v) (es-numeric stx (syntax-e #'v))]
+    [(string v) (es-string stx (syntax-e #'v))]
+    [(primary-expression "null") (es-null stx)]
+    [(primary-expression "true") (es-bool stx #t)]
+    [(primary-expression "false") (es-bool stx #f)]
+    [(primary-expression "(" expr ")") (parse-expression #'expr)]
     [(comma-expression left "," right) (es-comma-expr stx (parse-expression #'left) (parse-expression #'right))]
-    [(_ e) (parse-expression #'e)]
     [(_ obj "[" prop "]") (es-member-expr stx (parse-expression #'obj) (parse-expression #'prop))]
     [(_ obj "." prop) (es-member-expr stx (parse-expression #'obj) (parse-id #'prop))]
     [(_ "new" expr args) (es-new-expr stx (parse-expression #'expr) (parse-arguments #'args))]
     [(_ "new" expr) (es-new-expr stx (parse-expression #'expr) '())]
     [(call-expression expr args) (es-call-expr stx (parse-expression #'expr) (parse-arguments #'args))]
     [(postfix-expression expr op) (es-postfix-expr stx (parse-expression #'expr) (string->symbol (syntax-e #'op)))]
+    [(unary-expression op expr) (es-unary-expr stx (string->symbol (syntax-e #'op)) (parse-expression #'expr))]
     [(assignment-expression left (assignment-operator op) right)
      (es-binary-expr stx (parse-expression #'left) (string->symbol (syntax-e #'op)) (parse-expression #'right))]
     [(_ left op:str right)
      (es-binary-expr stx (parse-expression #'left) (string->symbol (syntax-e #'op)) (parse-expression #'right))]
     [(conditional-expression test "?" true ":" false)
      (es-ternary-expr stx (parse-expression #'test) (parse-expression #'true) (parse-expression #'false))]))
-
-(define (parse-primary-expression stx)
-  (syntax-parse stx
-    #:datum-literals (primary-expression identifier numeric string)
-    [(primary-expression "this") (es-this stx)]
-    [(primary-expression (identifier id)) (es-id #'id (syntax-e #'id))]
-    [(primary-expression (numeric v)) (es-numeric stx (syntax-e #'v))]
-    [(primary-expression (string v)) (es-string stx (syntax-e #'v))]
-    [(primary-expression "null") (es-null stx)]
-    [(primary-expression "true") (es-bool stx #t)]
-    [(primary-expression "false") (es-bool stx #f)]
-    [(primary-expression "(" expr ")") (parse-expression #'expr)]))
 
 (define (parse-id stx)
   (syntax-parse stx
