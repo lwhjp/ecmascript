705f67c311605d2c1e5f753ef64b91aa6f8dbda2
Leo Uino <leo@norisys.jp>
2014-10-13 16:33:49 +0900

    Prefix statement definitions

--- private/statement.rkt
+++ private/statement.rkt
@@ -2,46 +2,43 @@
 
 (require (for-syntax racket/base
                      syntax/parse)
+         racket/provide
          racket/stxparam
          "environment.rkt")
 
-(provide break
-         continue
-         block
-         empty-statement
-         while
-         with
-         (rename-out
-          [eif if]
-          [efor for]))
+(provide (filtered-out
+          (λ (name)
+            (and (regexp-match? #rx"^stmt:" name)
+                 (substring name 5)))
+          (all-defined-out)))
 
-(define-syntax-parameter break
+(define-syntax-parameter stmt:break
   (λ (stx)
     (raise-syntax-error #f "invalid outside of loop" stx)))
 
-(define-syntax-parameter continue
+(define-syntax-parameter stmt:continue
   (λ (stx)
     (raise-syntax-error #f "invalid outside of loop" stx)))
 
-(define-syntax-rule (block stmt0 stmt ...)
+(define-syntax-rule (stmt:block stmt0 stmt ...)
   (begin
     stmt0 stmt ...))
 
-(define (empty-statement)
+(define (stmt:empty-statement)
   (void))
 
-(define-syntax (eif stx)
+(define-syntax (stmt:if stx)
   (syntax-case stx ()
     [(_ test true)
      #'(if test true (void))]
     [(_ test true false)
      #'(if test true false)]))
 
-(define-syntax-rule (while test body0 body ...)
-  (efor #:test test
+(define-syntax-rule (stmt:while test body0 body ...)
+  (stmt:for #:test test
     body0 body ...))
 
-(define-syntax (efor stx)
+(define-syntax (stmt:for stx)
   (syntax-parse stx
     [(_ (~optional (~seq #:init init))
         (~optional (~seq #:test test))
@@ -63,6 +60,6 @@
               #,(or (attribute update) '(void))
               new-rv))))]))
 
-(define-syntax-rule (with expr body0 body ...)
+(define-syntax-rule (stmt:with expr body0 body ...)
   (begin-scope expr
     body0 body ...))
