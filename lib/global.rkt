e79a2f1409b2d3020de3192f11aa2f5107e6199b
Leo Uino <leo@norisys.jp>
2014-10-13 17:03:42 +0900

    Don't double-wrap properties of global object

--- lib/global.rkt
+++ lib/global.rkt
@@ -16,8 +16,8 @@
 ;; TODO: array
 
 (define-object-properties global-object
-  ["NaN" (make-data-property +nan.0)]
-  ["Infinity" (make-data-property +inf.0)]
+  ["NaN" +nan.0]
+  ["Infinity" +inf.0]
   ; TODO: eval
   ; TODO: parseInt
   ; TODO: parseFloat
@@ -25,11 +25,11 @@
   ; TODO: unescape
   ; TODO: isNaN
   ; TODO: isFinite
-  ["Object" (make-data-property object-constructor)]
-  ["Function" (make-data-property function-constructor)]
-  ; ("Array" (make-data-property array-constructor))
-  ["String" (make-data-property string-constructor)]
-  ["Boolean" (make-data-property boolean-constructor)]
-  ; ("Number" . ,(make-data-property number-constructor))
-  ; ("Date" . ,(make-data-property date-constructor))
-  ["Math" (make-data-property math)])
+  ["Object" object-constructor]
+  ["Function" function-constructor]
+  ; ["Array" array-constructor]
+  ["String" string-constructor]
+  ["Boolean" boolean-constructor]
+  ; ["Number" number-constructor]
+  ; ["Date" date-constructor]
+  ["Math" math])
