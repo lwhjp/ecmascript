4f58c17bea03208f49e7020a6f0735549db2ad0c
Leo Uino <leo@norisys.jp>
2014-10-14 19:03:56 +0900

    Properties for object prototype

--- lib/object.rkt
+++ lib/object.rkt
@@ -30,4 +30,49 @@
   ["prototype" object-prototype])
 
 (define-object-properties object-prototype
-  ["constructor" object-constructor])
+  ["constructor" object-constructor]
+  ["toString"
+   (make-native-function
+    (λ (this)
+      (cond
+        [(eq? 'undefined this) "[object Undefined]"]
+        [(eq? 'null this) "[object Null]"]
+        [(format "[object ~a]"
+                 (get-field class (to-object this)))])))]
+  ["toLocaleString"
+   (make-native-function
+    (λ (this)
+      (define o (to-object this))
+      (define f (send o get "toString"))
+      (unless (is-a? f function%)
+        (error "type error"))
+      (send f call o)))]
+  ["valueOf"
+   (make-native-function
+    (λ (this)
+      (to-object this)))]
+  ["hasOwnProperty"
+   (make-native-function
+    (λ (this v)
+      (property?
+       (send (to-object this)
+             get-own-property
+             (to-string v)))))]
+  ["isPrototypeOf"
+   (make-native-function
+    (λ (this v)
+      (and (is-a? v ecma-object%)
+           (let ([o (to-object this)])
+             (let loop ([v (get-field prototype v)])
+               (and (not (eq? 'null v))
+                    (or (eq? o v)
+                        (loop (get-field prototype v)))))))))]
+  ["propertyIsEnumerable"
+   (make-native-function
+    (λ (this v)
+      (define prop
+        (send (to-object this)
+              get-own-property
+              (to-string v)))
+      (and (property? prop)
+           (property-enumerable? prop))))])
