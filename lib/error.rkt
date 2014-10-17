e1e904dcb530f3ae00fc7a03e054ccded3a4402b
Leo Uino <leo@norisys.jp>
2014-10-17 22:58:40 +0900

    Move error objects into lib

--- lib/error.rkt
+++ lib/error.rkt
@@ -1,12 +1,78 @@
 #lang racket/base
 
-(require racket/class
+(require (for-syntax racket/base
+                     racket/syntax)
+         racket/class
          "../private/error.rkt"
          "../private/function.rkt"
          "../private/object.rkt"
          "../private/types.rkt")
 
 (provide (all-defined-out))
+
+(define error%
+  (class ecma-object%
+    (super-new [class "Error"])))
+
+(define (make-error-prototype+constructor name super-prototype)
+  (let ([prototype
+         (new error%
+              [prototype super-prototype])]
+        [constructor
+         (letrec
+             ([call
+               (λ (this . args)
+                 (apply construct args))]
+              [construct
+               (λ (message)
+                 (new error%
+                      [prototype function-prototype]
+                      [initial-properties
+                       (if (eq? 'undefined message)
+                           '()
+                           `(("message" . ,(make-data-property
+                                            (to-string message)))))]))])
+           (make-native-constructor call construct))])
+    (define-object-properties prototype
+      ["constructor" constructor]
+      ["name" name]
+      ["message" ""])
+    (define-object-properties constructor
+      ["prototype" prototype])
+    (values prototype constructor)))
+
+(define-values (error-prototype error-constructor)
+  (make-error-prototype+constructor "Error" object-prototype))
+
+(define-syntax (define-native-error stx)
+  (syntax-case stx ()
+    [(_ base-str)
+     (let ([base-name (string->symbol
+                       (string-append
+                        (string-downcase
+                         (syntax-e #'base-str))
+                        "-error"))])
+       (with-syntax
+         ([proto-id (format-id stx "~a-prototype" base-name)]
+          [cons-id (format-id stx "~a-constructor" base-name)]
+          [throw-id (format-id stx "throw-~a" base-name)])
+         #'(define-values (proto-id cons-id throw-id)
+             (let-values
+                 ([(proto cons)
+                   (make-error-prototype+constructor
+                    (string-append base-str "Error")
+                    error-prototype)])
+               (values proto
+                       cons
+                       (λ (msg)
+                         (send cons-id new msg)))))))]))
+
+(define-native-error "Eval")
+(define-native-error "Range")
+(define-native-error "Reference")
+(define-native-error "Syntax")
+(define-native-error "Type")
+(define-native-error "URI")
 
 (define-object-properties error-prototype
   ["toString"
@@ -28,3 +94,13 @@
         [(string=? "" name) msg]
         [(string=? "" msg) name]
         [else (string-append name ": " msg)])))])
+
+(native-error-handler
+ (λ (type message)
+   (case type
+     [(range) (throw-range-error message)]
+     [(reference) (throw-reference-error message)]
+     [(syntax) (throw-syntax-error message)]
+     [(type) (throw-type-error message)]
+     [(uri) (throw-uri-error message)]
+     [else (error 'native-error-handler "unknown error type: ~a" type)])))
