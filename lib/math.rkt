6ca6d7906775c111f2f57304bcd481f1366fa69e
Leo Uino <leo@norisys.jp>
2014-10-20 17:10:28 +0900

    Fill in Math object

--- lib/math.rkt
+++ lib/math.rkt
@@ -1,7 +1,10 @@
 #lang racket/base
 
 (require racket/class
-         "../private/object.rkt")
+         racket/math
+         "../private/function.rkt"
+         "../private/object.rkt"
+         "../private/types.rkt")
 
 (provide get-properties)
 
@@ -11,6 +14,36 @@
 (define math
   (new ecma-object%
     [prototype object-prototype]
-    [class "Math"]
-    [initial-properties
-     `(#| TODO |#)]))
+    [class "Math"]))
+
+(define-syntax-rule (wrap-math fn arg ...)
+  (native-method (this arg ...)
+    (fn (to-number arg) ...)))
+
+(define-object-properties math
+  ["E" (exp 1)]
+  ["LN10" (log 10)]
+  ["LN2" (log 2)]
+  ["LOG2E" (/ (log 2))]
+  ["LOG10E" (/ (log 10))]
+  ["PI" pi]
+  ["SQRT1_2" (sqrt 1/2)]
+  ["SQRT2" (sqrt 2)]
+  ["abs" (wrap-math abs x)]
+  ["acos" (wrap-math acos x)]
+  ["asin" (wrap-math asin x)]
+  ["atan" (wrap-math atan x)]
+  ["atan2" (wrap-math atan y x)]
+  ["ceil" (wrap-math ceiling x)]
+  ["cos" (wrap-math cos x)]
+  ["exp" (wrap-math exp x)]
+  ["floor" (wrap-math floor x)]
+  ["log" (wrap-math log x)]
+  ["max" (wrap-math max v1 v2)]
+  ["min" (wrap-math min v1 v2)]
+  ["pow" (wrap-math expt x y)]
+  ["random" (wrap-math random)]
+  ["round" (wrap-math round x)]
+  ["sin" (wrap-math sin x)]
+  ["sqrt" (wrap-math sqrt x)]
+  ["tan" (wrap-math tan x)])
